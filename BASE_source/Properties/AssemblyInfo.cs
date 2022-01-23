41282bb01a837a4f8d5db50750ca78c68afe1529
PieterVanZyl <36800800+PieterVanZyl@users.noreply.github.com>
2022-01-23 21:29:11 +0200

    Base updates

--- BASE_source/Properties/AssemblyInfo.cs
+++ BASE_source/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -6,9 +7,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("BASE")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Benchmark Appraiser Support Environment")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Demix (Pty) Ltd")]
 [assembly: AssemblyProduct("BASE")]
 [assembly: AssemblyCopyright("Copyright ©  2020,2021 Demix (Pty) Ltd")]
 [assembly: AssemblyTrademark("")]
@@ -32,6 +33,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.8.1.58")]
+[assembly: AssemblyVersion("3.8.1.59")]
+[assembly: NeutralResourcesLanguage("en")]
 //[assembly: AssemblyVersion("1.0.0.0")]
 //[assembly: AssemblyFileVersion("1.0.0.0")]
