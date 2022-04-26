7143efb1dd7a1d5f97fde5afdc4c364eef593e6b
PieterVanZyl <36800800+PieterVanZyl@users.noreply.github.com>
2022-04-26 20:37:43 +0200

    Version 12 updates

---
+++ BASE_install/Application Files/BASE_4_1_1_10/signBat.bat
@@ -0,0 +1,1 @@
+signtool.exe sign /fd sha256 /v /n "Demix" /tr "http://timestamp.digicert.com" /td sha256 "BASE.application"
---
+++ BASE_install/signManifest.bat
@@ -0,0 +1,1 @@
+signtool.exe sign /fd sha256 /v /n "Demix" /tr "http://ts.ssl.com" /td sha256 "C:\Users\pwjva\Documents\GitHub\CMMITools\BASE_install\BASE.application"
