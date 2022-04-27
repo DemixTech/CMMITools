1d1542b906689188829abb700f359c2d05908423
PieterVanZyl <36800800+PieterVanZyl@users.noreply.github.com>
2022-04-27 05:41:15 +0200

    Version 8

--- BASE_install/Application Files/BASE_4_1_1_10/signBat.bat
+++
@@ -1,1 +0,0 @@
-signtool.exe sign /fd sha256 /v /n "Demix" /tr "http://timestamp.digicert.com" /td sha256 "BASE.application"
--- BASE_install/signManifest.bat
+++
@@ -1,1 +0,0 @@
-signtool.exe sign /fd sha256 /v /n "Demix" /tr "http://ts.ssl.com" /td sha256 "C:\Users\pwjva\Documents\GitHub\CMMITools\BASE_install\BASE.application"
